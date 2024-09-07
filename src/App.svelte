e72ff26290b6f50259d1a8e1c39097d12016dcbd
yonerwinr <ymrodriguez@urbe.edu.ve>
2024-09-07 13:36:31 -0400

    Fondo animado

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { Router, Route } from "svelte-routing";
   import AdminHome from "./lib/AdminHome.svelte"
   import FormComponent from "./lib/Form.svelte"
+    import Inicio from "./lib/Inicio.svelte";
   export let url = "";
 </script>
 
@@ -9,5 +10,7 @@
   <div class="container mx-auto m-3">
     <Route path="/admin" component={AdminHome} />
     <Route path="/" component={FormComponent}/>
+    <Route path="/inicio" component={Inicio}/>
+    
   </div>
 </Router>
