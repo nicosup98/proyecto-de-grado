cca6b194ef657ecfba57e6f25383a697c08af5f8
nicolangelo damico <damiconicola98@gmail.com>
2024-09-19 11:57:05 -0400

    ajustes en la vista para que vea mejor

--- src/App.svelte
+++ src/App.svelte
@@ -12,6 +12,30 @@
 </script>
 
 <Router {url}>
+
+<div class="ocean">
+    <div class="bubble bubble--1"></div>
+    <div class="bubble bubble--2"></div>
+    <div class="bubble bubble--3"></div>
+    <div class="bubble bubble--4"></div>
+    <div class="bubble bubble--5"></div>
+    <div class="bubble bubble--6"></div>
+    <div class="bubble bubble--7"></div>
+    <div class="bubble bubble--8"></div>
+    <div class="bubble bubble--9"></div>
+    <div class="bubble bubble--10"></div>
+    <div class="bubble bubble--11"></div>
+    <div class="bubble bubble--12"></div>
+</div>
+
+<nav class="sticky top-0 left-0 w-full bg-white bg-opacity-60 text-white p-2 flex justify-between items-center z-10">
+    <div class="flex items-center">
+        <img src="/urbe_logo.png" class="w-16 zoom-img mr-2" alt="URBE Logo" />
+    </div>
+    <div class="flex items-center">
+        <img src="/huella_logo.png" class="w-16 zoom-img ml-2" alt="Huella logo" />
+    </div>
+</nav>
   <div class="container mx-auto m-3">
     <Route path="/admin" component={AdminHome} />
     <Route path="/" component={FormComponent}/>
@@ -22,4 +46,9 @@
     <Route path="/prueba" component={Prueba}/>
     <Route path="/personal" component={Personal}/>
   </div>
+
+<footer class="bg-gray-800 text-white p-4 text-center w-full  bottom-0 left-0">
+  <p>Universidad Privada Dr. Rafael Belloso Chacín.</p>
+  <p>©2024 Powered by Servieduca All rights reserved</p>
+</footer>
 </Router>
--- src/lib/Inicio.svelte
+++ src/lib/Inicio.svelte
@@ -23,30 +23,7 @@
   }
 </style>
 
-<div class="ocean">
-  <div class="bubble bubble--1"></div>
-  <div class="bubble bubble--2"></div>
-  <div class="bubble bubble--3"></div>
-  <div class="bubble bubble--4"></div>
-  <div class="bubble bubble--5"></div>
-  <div class="bubble bubble--6"></div>
-  <div class="bubble bubble--7"></div>
-  <div class="bubble bubble--8"></div>
-  <div class="bubble bubble--9"></div>
-  <div class="bubble bubble--10"></div>
-  <div class="bubble bubble--11"></div>
-  <div class="bubble bubble--12"></div>
-</div>
-<link rel="stylesheet" href="fondo.css">
 
-<nav class="fixed top-0 left-0 w-full bg-white bg-opacity-60 text-white p-2 flex justify-between items-center z-10">
-  <div class="flex items-center">
-    <img src="urbe_logo.png" class="w-16 zoom-img mr-2" alt="URBE Logo" />
-  </div>
-  <div class="flex items-center">
-    <img src="sustentable_logo.png" class="w-16 zoom-img ml-6" alt="Huella logo" />
-  </div>
-</nav>
 
 <div class="flex flex-col md:flex-row justify-between items-center h-[calc(100vh-80px)] relative z-1 pt-16 overflow-hidden">
   <div class="text-left w-full md:w-1/3 p-6">
@@ -83,8 +60,3 @@
     </div>
   </form>
 </div>
-
-<footer class="bg-gray-800 text-white p-4 text-center w-full fixed bottom-0 left-0">
-  <p>Universidad Privada Dr. Rafael Belloso Chacín.</p>
-  <p>©2024 Powered by Servieduca All rights reserved</p>
-</footer>
