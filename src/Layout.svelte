919717b8e64301b746a98c3fd6824b066f3fda7f
yonerwinr <ymrodriguez@urbe.edu.ve>
2024-09-21 09:23:20 -0400

    Redireciones de Logos, urbe a urbe.edu y Urbe Sustentable a urbesustentable.urbe.edu

--- src/Layout.svelte
+++ src/Layout.svelte
@@ -16,12 +16,16 @@
     
     <nav class="sticky top-0 left-0 w-full bg-white bg-opacity-60 text-white p-2 flex justify-between items-center z-20">
         <div class="flex items-center">
+            <a href="https://www.urbe.edu" target="_blank">
             <img src="/urbe_logo.png" class="w-16 zoom-img mr-2" alt="URBE Logo" />
+            </a>
         </div>
         <div class="flex items-center">
-            <img src="/huella_logo.png" class="w-16 zoom-img ml-2" alt="Huella logo" />
+            <a href="https://urbesustentable.urbe.edu/" target="_blank">
+            <img src="/sustentable_logo.png" class="w-16 zoom-img ml-2" alt="Huella logo" />
+            </a>
         </div>
-    </nav>
+        </nav>
     <slot/>
     <footer class="bg-gray-800 text-white p-4 text-center w-full  bottom-0 left-0">
         <p>Universidad Privada Dr. Rafael Belloso Chacín.</p>
