6eb5f91535df788e8d1bc7b7668fae62cb9b8f29
yonerwinr <ymrodriguez@urbe.edu.ve>
2024-09-25 13:58:49 -0400

    Redirecion del boton continuar scroll de respuestas en caso de que aparezca una respuesta especifica

--- src/lib/Respuestas.svelte
+++ src/lib/Respuestas.svelte
@@ -41,26 +41,18 @@
 
     function handleSubmit(event) {
         event.preventDefault();
-        // Aquí puedes agregar la lógica para redirigir a otra página si es necesario
+        window.location.href = "https://urbesustentable.urbe.edu";
     }
 </script>
 
-<style>
-    @media (max-width: 350px) {
-        .responsive-padding {
-            padding-bottom: 32px;
-        }
-    }
-</style>
-
-<div class="flex justify-center items-center h-svh  z-1">
+<div class="flex justify-center items-center h-screen z-1">
     <div class="flex flex-col md:flex-row w-full md:w-2/3 max-w-4xl">
         <div class="w-full p-4 md:w-1/2">
             <h1 class="text-3xl font-bold text-white mb-4 text-left mt-8 ml-0">Título del Formulario</h1>
             <p class="text-lg text-white text-left ml-0 mb-8">Esta es una breve descripción del formulario. Por favor, completa la información requerida en el formulario a la derecha.</p>
         </div>
         <div class="w-full md:w-1/2">
-            <form on:submit={handleSubmit} class="bg-white bg-opacity-20 backdrop-blur-md border border-white border-opacity-30 p-10 rounded-lg w-full" style="max-height: 65rem; overflow-y: auto;">
+            <form on:submit={handleSubmit} class="bg-white bg-opacity-20 backdrop-blur-md border border-white border-opacity-30 p-10 rounded-lg w-full max-h-[65vh] overflow-y-auto">
                 <div class="relative z-10 mb-4 text-center">
                     <h2 class="text-2xl font-bold text-white">Respuestas</h2>
                 </div>
@@ -109,14 +101,12 @@
                 <hr class="border-t border-gray-300 my-4">
                 {/if}
 
-             
-
                 <div class="flex flex-col items-center">
                     <p class="text-lg mb-2 text-white text-center">{recommendation}</p>
                 </div>
 
                 <div class="flex justify-center">
-                    <button type="submit" class="bg-blue-500 text-white p-2 rounded-lg mt-4 hover:bg-blue-700 transition-colors duration-300 w-full">Continuar</button>
+                    <button type="submit" class="bg-blue-500 text-white p-2 rounded-lg mt-4 hover:bg-blue-700 transition-colors duration-300 w-full">Finalizar</button>
                 </div>
             </form>
         </div>
