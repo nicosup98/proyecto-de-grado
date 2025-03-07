80ff3eb6632980816763d7ac7cd759826d653e55
yonerwinr <ymrodriguez@urbe.edu.ve>
2025-03-07 07:45:10 -0400

    Actualizaciones del Login

--- src/lib/Admin.svelte
+++ src/lib/Admin.svelte
@@ -1,71 +1,88 @@
 <script>
   import { navigate } from "svelte-routing";
-import { login } from "../services/admin";
+  import { login } from "../services/admin";
 
-let user = {}
+  let user = {};
+  let showPassword = false;
 
-async function handleLogin(e) {
-  e.preventDefault()
-  const resp = await login(user);
+  async function handleLogin(e) {
+    e.preventDefault();
+    const resp = await login(user);
 
-  if(!resp.ok) {
-    alert('ocurrio un error al hacer login')
-    console.error(await resp.text())
-    return
+    if (!resp.ok) {
+      const errorText = await resp.text();
+      if (errorText.includes("invalid email")) {
+        alert("El correo es inválido");
+      } else if (errorText.includes("incorrect password")) {
+        alert("La contraseña es incorrecta");
+      } else {
+        alert("Ingrese el correo y la contraseña correctamente");
+      }
+      console.error(errorText);
+      return;
+    }
+
+    const data = await resp.text();
+
+    localStorage.setItem("token", data);
+    localStorage.setItem("email", user.email);
+
+    navigate("/administrador");
   }
-
-  const data = await resp.text()
-
-  localStorage.setItem('token',data)
-  localStorage.setItem('email',user.email)
-
-  navigate('/administrador')
-}
 </script>
 
-<!-- por que mano ??? -->
 <link
-href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
-rel="stylesheet"
+  href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+  rel="stylesheet"
 />
 <div
-class="flex justify-center items-center min-h-screen bg-cover bg-center"
-style="background-image: url('bg.jpg');"
+  class="flex justify-center items-start min-h-screen bg-cover bg-center"
+  style="background-image: url('bg.jpg');"
 >
-<div
-  class="wrapper bg-white bg-opacity-20 border-2 border-white border-opacity-20 backdrop-blur-md text-white rounded-lg p-8 w-96"
->
-  <form action="" on:submit={handleLogin}>
-    <h1 class="text-3xl text-center mb-6">Inicio De Sesión</h1>
-    <div class="input-box relative w-full h-12 mb-8">
-      <input
-        type="text"
-        placeholder="Usuario"
-        bind:value={user.email}
-        required
-        class="w-full h-full bg-white bg-opacity-20 border-none outline-none border-2 border-white border-opacity-20 rounded-full text-white placeholder-white px-5 py-2.5"
-      />
-      <i
-        class="bx bxs-user absolute right-5 top-1/2 transform -translate-y-1/2 text-xl"
-      ></i>
+  <div
+    class="wrapper bg-white bg-opacity-20 border-2 border-white border-opacity-20 backdrop-blur-md text-white rounded-lg p-8 w-96 mt-10"
+  >
+    <div class="image-container w-full h-48 mb-4">
+      <img src="public\organismos-operadores.jpeg" alt="Imagen" class="w-full h-full object-cover rounded-t-lg" />
     </div>
-    <div class="input-box relative w-full h-12 mb-8">
-      <input
-        type="password"
-        placeholder="Contraseña"
-        required
-        bind:value={user.password}
-        class="w-full h-full bg-white bg-opacity-20 border-none outline-none border-2 border-white border-opacity-20 rounded-full text-white placeholder-white px-5 py-2.5"
-      />
-      <i
-        class="bx bxs-lock-alt absolute right-5 top-1/2 transform -translate-y-1/2 text-xl"
-      ></i>
-    </div>
-    <button
-      type="submit"
-      class="btn w-full h-12 bg-green-300 border-none outline-none rounded-full shadow-md cursor-pointer text-lg text-gray-800 font-semibold transition-transform duration-300 transform hover:scale-105"
-      >Iniciar Sesión</button
-    >
-  </form>
+    <form action="" on:submit={handleLogin}>
+      <h1 class="text-3xl text-center mb-6">Inicio De Sesión</h1>
+      <div class="input-box relative w-full h-12 mb-8">
+        <input
+          type="text"
+          placeholder="Usuario"
+          bind:value={user.email}
+          required
+          class="w-full h-full bg-white bg-opacity-20 border-none outline-none border-2 border-white border-opacity-20 rounded-full text-white placeholder-white px-5 py-2.5"
+        />
+        <i
+          class="bx bxs-user absolute right-5 top-1/2 transform -translate-y-1/2 text-xl"
+        ></i>
+      </div>
+      <div class="input-box relative w-full h-12 mb-8">
+        {#if showPassword}
+        <input type="text" placeholder="Contraseña" required bind:value={user.password} class="w-full h-full bg-white bg-opacity-20 border-none outline-none border-2 border-white border-opacity-20 rounded-full text-white placeholder-white px-5 py-2.5" />
+        {:else}
+        <input type="password" placeholder="Contraseña" required bind:value={user.password} class="w-full h-full bg-white bg-opacity-20 border-none outline-none border-2 border-white border-opacity-20 rounded-full text-white placeholder-white px-5 py-2.5" />
+        {/if}
+        <button
+          type="button"
+          class="bx {showPassword ? 'bxs-show' : 'bxs-hide'} absolute right-5 top-1/2 transform -translate-y-1/2 text-xl cursor-pointer"
+          on:click={() => (showPassword = !showPassword)}
+          aria-label="Toggle password visibility"
+        ></button>
+      </div>
+      <button
+        type="submit"
+        class="btn w-full h-12 bg-green-300 border-none outline-none rounded-full shadow-md cursor-pointer text-lg text-gray-800 font-semibold transition-transform duration-300 transform hover:scale-105"
+        >Iniciar Sesión</button
+      >
+    </form>
+  </div>
 </div>
-</div>
+
+<style>
+  .image-container {
+    height: 200px; /* Ajusta la altura según sea necesario */
+  }
+</style>
